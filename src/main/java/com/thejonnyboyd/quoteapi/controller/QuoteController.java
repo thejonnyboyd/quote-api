7a61fc4c04b328f9e57f14bedce421df359ce535
thejonnyboyd <jboyd7011@gmail.com>
2025-04-16 22:18:18 +0100

    implemented quote controller class - working as expected

--- src/main/java/com/thejonnyboyd/quoteapi/controller/QuoteController.java
+++ src/main/java/com/thejonnyboyd/quoteapi/controller/QuoteController.java
@@ -1,4 +1,17 @@
 package com.thejonnyboyd.quoteapi.controller;
 
+import com.thejonnyboyd.quoteapi.model.Quote;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/quotes")
 public class QuoteController {
+
+    @GetMapping("/random")
+    public Quote getRandomQuote(){
+        return new Quote("Stay hungry, stay foolish", "Steve Jobs");
+    }
+
 }
