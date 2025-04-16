a3e3d05a62ad7ba84d46ce51b0d221d614146453
thejonnyboyd <jboyd7011@gmail.com>
2025-04-16 22:05:24 +0100

    implemented main application class

--- src/main/java/com/thejonnyboyd/quoteapi/QuoteAPIApplication.java
+++ src/main/java/com/thejonnyboyd/quoteapi/QuoteAPIApplication.java
@@ -1,4 +1,11 @@
 package com.thejonnyboyd.quoteapi;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class QuoteAPIApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuoteAPIApplication.class, args);
+    }
 }
